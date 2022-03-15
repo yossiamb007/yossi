30e80a110f7b010c0a23a576db375fd204261cbd
yossiamb007 <101555760+yossiamb007@users.noreply.github.com>
2022-03-15 09:23:59 +0200

    Add files via upload

--- loop_dir.py
+++ loop_dir.py
@@ -6,7 +6,7 @@ from stat import S_ISDIR, S_ISREG
 import sys
 
 
-def listdir_r(sftp, remotedir):
+def listdir_rec(sftp, remotedir):
     for entry in sftp.listdir_attr(remotedir):
         remotepath = remotedir + "/" + entry.filename
         mode = entry.st_mode
@@ -18,19 +18,19 @@ def listdir_r(sftp, remotedir):
 host_ip = sys.argv[1]
 username = sys.argv[2]
 password = sys.argv[3]
-path = sys.argv[4]
+dir_path = sys.argv[4]
 
 ssh = paramiko.SSHClient()
 # Load SSH host keys.
-ssh.load_system_host_keys()
+#ssh.load_system_host_keys()
 # Add SSH host key automatically if needed.
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 # Connect to router using username/password authentication.
 ssh.connect(host_ip,
             username=username,
             password=password,
-            look_for_keys=False )
+            look_for_keys=False)
 
 sftp_client = ssh.open_sftp()
 
-listdir_r(sftp_client,path)
+listdir_rec(sftp_client,dir_path)
