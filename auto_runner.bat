a3fc49f1bfee669b3ceca5ad28aa1c725bc46f2a
Yossi Ambelo <yossi.a@prom-ai.com>
2024-07-22 14:16:45 +0300

    test alure

---
+++ auto_runner.bat
@@ -0,0 +1,26 @@
+@echo off
+REM Navigate to the Automation directory
+cd /d C:\Users\Yossi Ambelo\Jenkins_Agent\workspace\SanityAutomation
+
+python -m venv venv
+
+REM Activate the virtual environment
+call venv\Scripts\activate
+
+REM Install requirements
+pip install -r requirements.txt
+
+REM Run pytest with Allure report generation
+pytest -m happyflow --alluredir allure-results
+
+REM Serve the Allure report
+allure serve allure-results
+
+REM Deactivate the virtual environment
+deactivate
+
+REM Remove the virtual environment directory
+rmdir /s /q venv
+
+REM Pause to keep the command prompt open
+pause
